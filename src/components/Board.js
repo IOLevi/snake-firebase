7c94921a57c23081b283942ec3f2de68621ed182
IOLevi <337@holbertonschool.com>
2019-04-27 15:32:57 -0700

    created skeleton with initial board logic

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
 
 export default class Board extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {board: this.initBoard(),
+    }
+  }
+
+  initBoard = () => {
+    let data = [];
+
+    for (let i = 0; i < this.props.height; i++) {
+      data[i].push([])
+      for (let j = 0; j < this.props.width; j++) {
+        data[i][j] = {x: i, y: j, sbody: false, food: false};
+      }
+
+    }
+    return data;
+  }
+
+  renderBoard = (data) => {
+    data.map(datarow => {
+      datarow.map(item => {
+        // conditionally fill if sbody is true
+        <div>
+          <Cell x={item.x} y={item.y}/>
+        </div>
+        console.log(`cell ${item.x} ${item.y}`);
+      }
+    }) 
+  }
   render() {
     return (
-      <div>
-        
-      </div>
+      this.renderBoard(this.state.board)
     )
   }
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
-
+import Board from "./Board"
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { height: 6, width: 6 };
+  }
+
   render() {
     return (
       <div>
-        <Board />
+        <Board height={this.state.height} width={this.state.width} />
       </div>
     )
   }
