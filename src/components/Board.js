8dc329237bd6a39ea81262df24ca12daf9e8e92c
IOLevi <337@holbertonschool.com>
2019-04-27 18:42:35 -0700

    working first version of animation

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,6 +7,7 @@ export default class Board extends Component {
     super(props);
     this.state = {
       board: this.initBoard(),
+      snake: this.initSnake(),
     }
     this.initBoard = this.initBoard.bind(this)
     this.renderBoard = this.renderBoard.bind(this)
@@ -22,24 +23,37 @@ export default class Board extends Component {
       }
 
     }
-    this.staticSnake(data);
+    // this.staticSnake(data);
     console.log(data)
     return data;
   }
 
-  staticSnake = (data) => {
-    data[2][2].snakeBody = true;
-    data[3][2].snakeBody = true;
-    data[4][2].snakeBody = true;
+  renderSnake = (data) => {
+    for (let segment of this.state.snake) {
+      data[segment.x][segment.y].snakeBody = true;
+    }
   }
 
-  dynamicSnake = () => {
+  initSnake = () => {
     let snake = [{ x: 2, y: 2 },
     { x: 3, y: 2 },
-    { x: 4, y: 2 }]
+    { x: 4, y: 2 }];
+    return snake;
+  }
+
+  animateSnake = () => {
+    // const newSnake = Object.assign({}, this.state.snake, {x: this.state.snake.x + 1})
+    const newSnake = this.state.snake.slice();
+    newSnake[0].y += 1;
+    this.setState({snake: newSnake});
+  }
+
+  componentDidMount = () => {
+    setInterval(this.animateSnake, 3000)
   }
 
   renderBoard = (data) => {
+    this.renderSnake(data);
     return data.map(datarow => {
       return datarow.map(item => {
         // conditionally fill if sbody is true
