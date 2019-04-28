2e9f41249886d18109083b4282052b25f36f6f4f
IOLevi <337@holbertonschool.com>
2019-04-27 19:24:56 -0700

    removing tail functional

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,14 +3,19 @@ import Cell from "./Cell"
 
 export default class Board extends Component {
 
+  snake = [{ x: 2, y: 2 },
+    { x: 3, y: 2 },
+    { x: 4, y: 2 }];
+
   constructor(props) {
     super(props);
     this.state = {
       board: this.initBoard(),
-      snake: this.initSnake(),
+      // snake: this.initSnake(),
     }
     this.initBoard = this.initBoard.bind(this)
     this.renderBoard = this.renderBoard.bind(this)
+    // this.initSnake = this.initSnake.bind(this)
   }
 
   initBoard = () => {
@@ -28,38 +33,49 @@ export default class Board extends Component {
     return data;
   }
 
-  renderSnake = (data) => {
-    for (let segment of this.state.snake) {
-      data[segment.x][segment.y].snakeBody = true;
-    }
+  // how to do this with a set state instead of looping and 
+  // changing this way
+  renderSnake = () => {
+    const newBoard = this.state.board.slice();
+    newBoard[this.snake[0].x][this.snake[0].y].snakeBody = false;
+    this.snake.shift();
+    // for (let segment of this.state.snake) {
+    //   newBoard[segment.x][segment.y].snakeBody = true;
+    // }
+    this.setState({ board: newBoard })
   }
 
   initSnake = () => {
-    let snake = [{ x: 2, y: 2 },
-    { x: 3, y: 2 },
-    { x: 4, y: 2 }];
-    return snake;
+    const newBoard = this.state.board.slice();
+    for (let segment of this.snake) {
+      newBoard[segment.x][segment.y].snakeBody = true;
+    }
+    this.setState({board:newBoard});
+    
   }
 
   animateSnake = () => {
     // const newSnake = Object.assign({}, this.state.snake, {x: this.state.snake.x + 1})
-    const newSnake = this.state.snake.slice();
-    newSnake[0].y += 1;
-    this.setState({snake: newSnake});
+    // const newSnake = this.state.snake.slice();
+    this.snake[0].y += 1;
+    // turn off the one before?!?!
+
+    // this.setState({snake: newSnake});
+
   }
 
   componentDidMount = () => {
-    setInterval(this.animateSnake, 3000)
+    this.initSnake();
+    setInterval(this.renderSnake, 3000)
   }
 
   renderBoard = (data) => {
-    this.renderSnake(data);
     return data.map(datarow => {
       return datarow.map(item => {
         // conditionally fill if sbody is true
         return (
           <div key={(item.x, item.y)}>
-          
+
             <Cell x={item.x} y={item.y} snakeBody={item.snakeBody} food={item.food} />
             {(datarow[datarow.length - 1] === item) ? <div className="clear" /> : ""}
           </div>
