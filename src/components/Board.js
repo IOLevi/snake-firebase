8c63a2bc79115ec21408c27c5f6971a364383311
IOLevi <337@holbertonschool.com>
2019-04-27 18:09:38 -0700

    fix css

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,9 +3,10 @@ import Cell from "./Cell"
 
 export default class Board extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {board: this.initBoard(),
+    this.state = {
+      board: this.initBoard(),
     }
     this.initBoard = this.initBoard.bind(this)
     this.renderBoard = this.renderBoard.bind(this)
@@ -17,7 +18,7 @@ export default class Board extends Component {
     for (let i = 0; i < this.props.height; i++) {
       data.push([])
       for (let j = 0; j < this.props.width; j++) {
-        data[i][j] = {x: i, y: j, snakeBody: false, food: false};
+        data[i][j] = { x: i, y: j, snakeBody: false, food: false };
       }
 
     }
@@ -28,6 +29,14 @@ export default class Board extends Component {
 
   staticSnake = (data) => {
     data[2][2].snakeBody = true;
+    data[3][2].snakeBody = true;
+    data[4][2].snakeBody = true;
+  }
+
+  dynamicSnake = () => {
+    let snake = [{ x: 2, y: 2 },
+    { x: 3, y: 2 },
+    { x: 4, y: 2 }]
   }
 
   renderBoard = (data) => {
@@ -35,14 +44,15 @@ export default class Board extends Component {
       return datarow.map(item => {
         // conditionally fill if sbody is true
         return (
-          <div key={(item.x,item.y)}>
-            <Cell x={item.x} y={item.y} snakeBody={item.snakeBody} food={item.food}/>
+          <div key={(item.x, item.y)}>
+          
+            <Cell x={item.x} y={item.y} snakeBody={item.snakeBody} food={item.food} />
             {(datarow[datarow.length - 1] === item) ? <div className="clear" /> : ""}
           </div>
         )
-    }) 
-  })
-}
+      })
+    })
+  }
 
   render() {
     return (
