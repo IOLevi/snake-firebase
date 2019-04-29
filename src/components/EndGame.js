2c0b23c10e107e545c9a4318b1d2443cb84215e9
IOLevi <337@holbertonschool.com>
2019-04-29 12:35:34 -0700

    added a test for entire game

--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
 
 export default class EndGame extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
   render() {
     return (
       <div>
         Game Over
-        <button onClick={this.props.gameRestart}></button>
+        <button className="button" onClick={this.props.gameRestart}>Restart</button>
       </div>
     )
   }
